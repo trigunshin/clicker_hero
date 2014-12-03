c29ef8cad7591c8fd14210b4b0c071f100ce30fc
pcrane <trigunshin@gmail.com>
2014-12-02 20:39:47 -0500

    Fixed reload flash applying to calc  rerun button.

--- static/js/init_a.js
+++ static/js/init_a.js
@@ -23,7 +23,7 @@ var calc_config_template_data = {};
 $('#calc_config_anchor').after(calc_config_template(calc_config_template_data));
 
 // set up on-click for game reload button
-$('div#reload_flash').click(function() {
+$('#reload_flash').click(function() {
      var clone = $('#flashContent object').clone();
       $('#flashContent object').remove();
      $('#flashContent').append(clone);
