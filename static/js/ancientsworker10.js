9a8eef9c45ab9e4d9f0f8fdc7d80f08cbb3c4397
pcrane <pcrane@goscoutgo.com>
2014-12-04 11:45:30 -0500

    Updated calc with new data from reddit
    Thanks to Limeox and rler
    
    From http://redd.it/2o84v1

--- static/js/ancientsworker10.js
+++ static/js/ancientsworker10.js
@@ -1,76 +1,41 @@
 var Heroes = [
-  {name: "Tree Beast", cost: 50, damage: 5 * 20, level: 100},
-  {name: "Ivan, the Drunken Brawler", cost: 250, damage: 22 * 20, level: 125},
-  {name: "Brittany, the Beach Princess", cost: 1000, damage: 74 * 20, level: 75},
-  {name: "The Wandering Fisherman", cost: 4000, damage: 245 * 8, level: 100},
-  {name: "Betty Clicker", cost: 20000, damage: 976, level: 100},
-  {name: "The Masked Samurai", cost: 100e3, damage: 3725 * 20, level: 75},
-  {name: "Leon", cost: 400e3, damage: 10859 * 8, level: 75},
-  {name: "The Great Forest Seer", cost: 2500e3, damage: 47143 * 20, level: 75},
-  {name: "Alexa, the Assassin", cost: 15000e3, damage: 186e3 * 5.0625, level: 100},
-  {name: "Natalia, Ice Apprentice", cost: 100e6, damage: 782e3 * 20, level: 75},
-  {name: "Mercedes, Duchess of Blades", cost: 800e6, damage: 3721e3 * 20, level: 100},
-  {name: "Bobby, Bounty Hunter", cost: 6500e6, damage: 17010e3 * 20, level: 100},
-  {name: "Broyle Lindoven, Fire Mage", cost: 50e9, damage: 69480e3 * 10, level: 100},
-  {name: "Sir George II, King's Guard", cost: 450e9, damage: 460e6 * 20, level: 100},
-  {name: "King Midas", cost: 4e12, damage: 3017e6, level: 125},
-  {name: "Referi Jerator, Ice Wizard", cost: 36e12, damage: 20009e6 * 20, level: 125},
-  {name: "Abaddon", cost: 320e12, damage: 131e9 * 11.390625, level: 75},
-  {name: "Ma Zhu", cost: 2.7e15, damage: 814e9 * 20, level: 75},
-  {name: "Amenhotep", cost: 24e15, damage: 5335e9 * 2, level: 50},
-  {name: "Beastlord", cost: 300e15, damage: 49143e9 * 8, level: 100},
-  {name: "Athena, Goddess of War", cost: 9e18, damage: 1086e12 * 16, level: 100},
-  {name: "Aphrodite, Goddess of Love", cost: 350e18, damage: 31124e12 * 16, level: 125},
-  {name: "Shinatobe, Wind Deity", cost: 14e21, damage: 917e15 * 8, level: 100},
-  {name: "Grant, the General", cost: 4199e21, damage: 202e18 * 4, level: 75},
-  {name: "Frostleaf", cost: 2100e24, damage: 74698e18 * 4, level: 75},
-  {name: "Dread Knight", cost: 1e40, damage: 146e30 * 20, level: 100},
-  {name: "Atlas", cost: 1e55, damage: 1075e42 * 20, level: 100},
-  {name: "Terra", cost: 1e70, damage: 7926e54 * 20, level: 100},
-  {name: "Phthalo", cost: 1e85, damage: 5839e67 * 20, level: 100},
-  {name: "Orntchya Gladeye, Didensy Banana", cost: 1e100, damage: 3302e80 * 20, level: 100},
-  {name: "Lilin", cost: 1e115, damage: 3170e93 * 20, level: 100},
-  {name: "Cadmia", cost: 1e130, damage: 2335e106 * 20, level: 100},
-  {name: "Alabaster", cost: 1e145, damage: 1721e119 * 20, level: 100},
-  {name: "Astraea", cost: 1e160, damage: 1268e132 * 20, level: 100},
+  /*{name: "Cid, the Helpful Adventurer", cost: 5, damage: 0, level: 0, upgrades: [100, 250, 1e3, 8e3, 80e3, 400e3, 4e6]}, */
+  {name: "Tree Beast", cost: 50, damage: 5 * 20, level: 100, upgrades: [500, 1.25e3, 5e3, 40e3, 400e3]},
+  {name: "Ivan, the Drunken Brawler", cost: 250, damage: 22 * 20, level: 125, upgrades: [2.5e3, 6.25e3, 25e3, 200e3, 2e6, 10e6]},
+  {name: "Brittany, the Beach Princess", cost: 1000, damage: 74 * 20, level: 75, upgrades: [10e3, 25e3, 100e3, 800e3]},
+  {name: "The Wandering Fisherman", cost: 4000, damage: 245 * 8, level: 100, upgrades: [40e3, 100e3, 400e3, 3.2e6, 32e6]},
+  {name: "Betty Clicker", cost: 20000, damage: 976, level: 100, upgrades: [200e3, 500e3, 2e6, 16e6, 160e6]},
+  {name: "The Masked Samurai", cost: 100e3, damage: 3725 * 20, level: 75, upgrades: [1e6, 2.5e6, 10e6, 80e6]},
+  {name: "Leon", cost: 400e3, damage: 10859 * 8, level: 75, upgrades: [4e6, 10e6, 40e6, 320e6]},
+  {name: "The Great Forest Seer", cost: 2500e3, damage: 47143 * 20, level: 75, upgrades: [25e6, 62.5e6, 250e6, 2e9]},
+  {name: "Alexa, the Assassin", cost: 15000e3, damage: 186e3 * 5.0625, level: 100, upgrades: [150e6, 375e6, 1.5e9, 12e9, 120e9]},
+  {name: "Natalia, Ice Apprentice", cost: 100e6, damage: 782e3 * 20, level: 75, upgrades: [1e9, 2.5e9, 10e9, 80e9]},
+  {name: "Mercedes, Duchess of Blades", cost: 800e6, damage: 3721e3 * 20, level: 100, upgrades: [8e9, 20e9, 80e9, 640e9, 6.4e12]},
+  {name: "Bobby, Bounty Hunter", cost: 6500e6, damage: 17010e3 * 20, level: 100, upgrades: [65e9, 162e9, 650e9, 5.2e12, 52e12]},
+  {name: "Broyle Lindoven, Fire Mage", cost: 50e9, damage: 69480e3 * 10, level: 100, upgrades: [500e9, 1.25e12, 5e12, 40e12, 400e12]},
+  {name: "Sir George II, King's Guard", cost: 450e9, damage: 460e6 * 20, level: 100, upgrades: [4.5e12, 11.25e12, 45e12, 360e12, 3.6e15]},
+  {name: "King Midas", cost: 4e12, damage: 3017e6, level: 125, upgrades: [40e12, 100e12, 400e12, 3.2e15, 32e15, 160e15]},
+  {name: "Referi Jerator, Ice Wizard", cost: 36e12, damage: 20009e6 * 20, level: 125, upgrades: [360e12, 900e12, 3.6e15, 28.8e15, 288e15]},
+  {name: "Abaddon", cost: 320e12, damage: 131e9 * 11.390625, level: 75, upgrades: [3.2e15, 8e15, 32e15, 256e15]},
+  {name: "Ma Zhu", cost: 2.7e15, damage: 814e9 * 20, level: 75, upgrades: [27e15, 67.5e15, 270e15, 2.16e18]},
+  {name: "Amenhotep", cost: 24e15, damage: 5335e9 * 2, level: 50, upgrades: [240e15, 600e15, 2.4e18 /*, 19.2e18*/]},
+  {name: "Beastlord", cost: 300e15, damage: 49143e9 * 8, level: 100, upgrades: [3e18, 7.5e18, 30e18, 240e18, 2.4e21]},
+  {name: "Athena, Goddess of War", cost: 9e18, damage: 1086e12 * 16, level: 100, upgrades: [90e18, 225e18, 900e18, 0, 7.2e21]},
+  {name: "Aphrodite, Goddess of Love", cost: 350e18, damage: 31124e12 * 16, level: 125, upgrades: [3.5e21, 8.75e21, 35e21, 0, 280e21, 2.8e24]},
+  {name: "Shinatobe, Wind Deity", cost: 14e21, damage: 917e15 * 8, level: 100, upgrades: [140e21, 350e21, 1.4e24, 11.2e24, 112e24]},
+  {name: "Grant, the General", cost: 4199e21, damage: 202e18 * 4, level: 75, upgrades: [41.999e24, 104e24, 419e24, 3.359e27]},
+  {name: "Frostleaf", cost: 2100e24, damage: 74698e18 * 4, level: 75, upgrades: [21e27, 52.499e27, 209e27, 1.679e30]},
+  {name: "Dread Knight", cost: 1e40, damage: 1.46e32 * 20, level: 0, has5x: true, upgrades: [1e41, 2.5e41, 1e42, 0, 8e42]},
+  {name: "Atlas", cost: 1e55, damage: 1.075e45 * 20, level: 0, has5x: true, upgrades: [1e56, 2.5e56, 1e57, 0, 8e57]},
+  {name: "Terra", cost: 1e70, damage: 7.926e57 * 20, level: 0, has5x: true, upgrades: [1e71, 2.5e71, 1e72, 0, 8e72]},
+  {name: "Phthalo", cost: 1e85, damage: 5.839e70 * 20, level: 0, has5x: true, upgrades: [1e86, 2.5e86, 1e87, 0, 8e87]},
+  {name: "Orntchya Gladeye, Didensy Banana", cost: 1e100, damage: 3.302e83 * 20, level: 0, has5x: true, upgrades: [1e101, 2.5e101, 1e102, 0, 8e102]},
+  {name: "Lilin", cost: 1e115, damage: 3.17e96 * 20, level: 0, has5x: true, upgrades: [1e116, 2.5e116, 1e117, 0, 8e117]},
+  {name: "Cadmia", cost: 1e130, damage: 2.335e109 * 20, level: 0, has5x: true, upgrades: [1e131, 2.5e131, 1e132, 0, 8e132]},
+  {name: "Alabaster", cost: 1e145, damage: 1.721e122 * 20, level: 0, has5x: true, upgrades: [1e146, 2.5e146, 1e147, 0, 8e147]},
+  {name: "Astraea", cost: 1e160, damage: 1.268e135 * 20, level: 0, has5x: true, upgrades: [1e161, 2.5e161, 1e162, 0, 8e162]}
 ];
-var TotalUpgradeCost =
-  100 + 250 + 1e3 + 8e3 + 80e3 + 400e3 + 4e6 + // cid
-  500 + 1.25e3 + 5e3 + 40e3 + 400e3 + // treebeast
-  2.5e3 + 6.25e3 + 25e3 + 200e3 + 2e6 + 10e6 + // ivan
-  10e3 + 25e3 + 100e3 + 800e3 + // brittany
-  40e3 + 100e3 + 400e3 + 3.2e6 + 32e6 + // wandering
-  200e3 + 500e3 + 2e6 + 16e6 + 160e6 + // betty
-  1e6 + 2.5e6 + 10e6 + 80e6 + // samurai
-  4e6 + 10e6 + 40e6 + 320e6 + // leon
-  25e6 + 62.5e6 + 250e6 + 2e9 + // forest seer
-  150e6 + 375e6 + 1.5e9 + 12e9 + 120e9 + // alexa
-  1e9 + 2.5e9 + 10e9 + 80e9 + // natalia
-  8e9 + 20e9 + 80e9 + 640e9 + 6.4e12 + // mercedes
-  65e9 + 162e9 + 650e9 + 5.2e12 + 52e12 + // bobby
-  500e9 + 1.25e12 + 5e12 + 40e12 + 400e12 + // broyle
-  4.5e12 + 11.25e12 + 45e12 + 360e12 + 3.6e15 + // sir george
-  40e12 + 100e12 + 400e12 + 3.2e15 + 32e15 + 160e15 + // king midas
-  360e12 + 900e12 + 3.6e15 + 28.8e15 + 288e15 + // referi
-  3.2e15 + 8e15 + 32e15 + 256e15 + // abaddon
-  27e15 + 67.5e15 + 270e15 + 2.16e18 + // ma zhu
-  240e15 + 600e15 + 2.4e18 + 19.2e18 + // amenhotep
-  3e18 + 7.5e18 + 30e18 + 240e18 + 2.4e21 + // beastlord
-  90e18 + 225e18 + 900e18 + 7.2e21 + // athena
-  3.5e21 + 8.75e21 + 35e21 + 280e21 + 2.8e24 + // aphrodite
-  140e21 + 350e21 + 1.4e24 + 11.2e24 + 112e24 + // shinatobe
-  41.999e24 + 104e24 + 419e24 + 3.359e27 + // grant
-  21e27 + 52.499e27 + 209e27 + 1.679e30 + // frostleaf
-  1e41 + 25e40 + 1e42 + 8e42 + // dread knight
-  1e56 + 25e55 + 1e57 + 8e57 + // atlas
-  1e71 + 25e70 + 1e72 + 8e72 + // terra
-  1e86 + 25e85 + 1e87 + 8e87 + // phthalo
-  1e101 + 25e100 + 1e102 + 8e102 + // orntchya
-  1e116 + 25e115 + 1e117 + 8e117 + // lilin
-  1e131 + 25e130 + 1e132 + 8e132 + // cadmia
-  1e146 + 25e145 + 1e147 + 8e147 + // alabaster
-  1e161 + 25e160 + 1e162 + 8e162 + // astraea
-  0;
+
 var BaseHSSouls = (Math.log(Heroes[33].cost / 20) / Math.log(1.07) + 1) / 2000;
 for (var i = 0; i < Heroes.length; i++) {
   var lvl = Math.log(Heroes[33].cost / Heroes[i].cost) / Math.log(1.07) + 1;
@@ -151,66 +116,106 @@ function LevelingPlan(argaiv, damageFactor) {
   this.Damage = function(hero, lvl) {
     var x10 = Math.min(Math.floor(lvl / 1000), 3);
     var x4 = Math.min(Math.max(Math.floor((lvl - 175) / 25), 0) - x10, 154);
-    return hero.damage * (1 + (0.5 + 0.02 * this.argaiv) * hero.gilded) * lvl * Math.pow(4, x4) * Math.pow(10, x10) * this.damageFactor;
+    var x5 = (hero.has5x ? Math.min(Math.max(Math.floor((lvl - 500) / 25), 0), 9) : 0);
+    return hero.damage * (1 + (0.5 + 0.02 * this.argaiv) * hero.gilded) * lvl * Math.pow(4, x4) * Math.pow(10, x10) * Math.pow(1.25, x5) * this.damageFactor;
   };
   this.Cost = function(hero, lvl) {
-    return hero.cost * Math.pow(1.07, lvl) / 0.07 * (1 - 0.02 * this.dogcog);
-  };
-  this.NextBest = function(hero, level, mined) {
-    var res = null;
-    var curdmg = this.Damage(hero, level);
-    var curcost = this.Cost(hero, level);
-    for (var i = level + 25; i <= 4100; i += 25) {
-      var ed = this.Damage(hero, i) - curdmg;
-      if (ed - curdmg < mined) {
-        continue;
-      }
-      var ec = this.Cost(hero, i) - curcost;
-      if (!res || ed / ec > res.ratio) {
-        res = {hero: hero, level: i, ratio: ed / ec, increase: ed - curdmg};
-      }
-    }
-    return res;
+    if (lvl == 0)
+      return 0;
+
+    var cost = hero.cost * Math.pow(1.07, lvl) / 0.07 * (1 - 0.02 * this.dogcog);
+    //Assume all upgrades are purchased to the current level
+    if (lvl >= 10 && hero.upgrades.length >= 1)
+      cost += hero.upgrades[0];
+    if (lvl >= 25 && hero.upgrades.length >= 2)
+      cost += hero.upgrades[1];
+    if (lvl >= 50 && hero.upgrades.length >= 3)
+      cost += hero.upgrades[2];
+    if (lvl >= 75 && hero.upgrades.length >= 4)
+      cost += hero.upgrades[3];
+    if (lvl >= 100 && hero.upgrades.length >= 5)
+      cost += hero.upgrades[4];
+    if (lvl >= 125 && hero.upgrades.length >= 6)
+      cost += hero.upgrades[5];
+    if (lvl >= 150 && hero.upgrades.length >= 7)
+      cost += hero.upgrades[6];
+
+    return cost;
   };
 
+  //Find the next level of the hero that will attain the given (mined) dps
+  this.NextBest = function(hero, level, mined) {
+    var curDmg = this.Damage(hero, level);
+    var curCost = this.Cost(hero, level);
+
+    for (var i = level + 25; i <= 4100; i += 25) {
+      var newDmg = this.Damage(hero, i);
+      if(newDmg < mined) {
+        continue;
+      }
+      var newCost = this.Cost(hero, i) - curCost;
+      var newRatio = (newDmg - curDmg) / (newCost - curCost);
+      return {hero: hero, level: i, ratio: newRatio, increase: newDmg - curDmg, costIncrease: newCost - curCost};
+    }
+    return null;
+  };
+
+  // result plan steps
   this.plan = [];
-  var bestList = [];
+  // cache next required level per hero
+  this.bestList = [];
   var curDamage = 0;
   var curLevels = [];
+  //at each plan step, we aim to increase the total dps to this multiplier
+  var dmgTargetRatio = 1.05;
+
+  //Compute baseline damage and levels
   for (var i = 0; i < Heroes.length; i++) {
     curDamage += this.Damage(Heroes[i], Heroes[i].level);
     curLevels.push(Heroes[i].level);
   }
+  //Compute baseline bestList
   for (var i = 0; i < Heroes.length; i++) {
-    bestList.push(this.NextBest(Heroes[i], Heroes[i].level, curDamage * 0.01));
+    this.bestList.push(this.NextBest(Heroes[i], Heroes[i].level, curDamage * dmgTargetRatio));
   }
+  // Compute plan steps
   while (true) {
     var nextBest = -1;
-    for (var i = 0; i < bestList.length; i++) {
-      if (bestList[i] && (nextBest < 0 || bestList[i].ratio > bestList[nextBest].ratio)) {
+    // find next target
+    for (var i = 0; i < this.bestList.length; i++) {
+      if (this.bestList[i] && (nextBest < 0 || this.bestList[i].costIncrease < this.bestList[nextBest].costIncrease)) {
         nextBest = i;
       }
     }
+    //Plan over, implies highest hero is maxed
     if (nextBest < 0) {
       break;
     }
-    if (bestList[nextBest].increase > curDamage * 0.01) {
-      while (curLevels[nextBest] < bestList[nextBest].level) {
-        curLevels[nextBest] += 25;
-        this.plan.push({hero: nextBest, level: curLevels[nextBest]});
-      }
-      curDamage += bestList[nextBest].increase;
+
+    //Add plan steps in batches of 25 levels up to the target
+    while (curLevels[nextBest] < this.bestList[nextBest].level) {
+      curLevels[nextBest] += 25;
+      this.plan.push({hero: nextBest, heroName: Heroes[nextBest].name, level: curLevels[nextBest]});
     }
-    bestList[nextBest] = this.NextBest(bestList[nextBest].hero, bestList[nextBest].level, curDamage * 0.01);
+    //Update target
+    curDamage += this.bestList[nextBest].increase;
+
+    //recompute bestList
+    for (var i = 0; i < Heroes.length; i++) {
+      if (this.bestList[i]) {
+        this.bestList[i] = this.NextBest(Heroes[i], Heroes[i].level, curDamage * dmgTargetRatio);
+      }
+    }
   }
 
+  //Fill in real computed plan damage and costs for each step
   this.Start = function(argaiv, dogcog) {
     this.argaiv = argaiv;
     this.dogcog = dogcog;
     this.planPos = 0;
     var heroes = [];
     var curDamage = 0;
-    var curCost = TotalUpgradeCost;
+    var curCost = 0;
     for (var i = 0; i < Heroes.length; i++) {
       var curHero = {damage: this.Damage(Heroes[i], Heroes[i].level),
                      cost: this.Cost(Heroes[i], Heroes[i].level)};
@@ -231,6 +236,9 @@ function LevelingPlan(argaiv, damageFactor) {
       this.plan[i].cost = curCost;
     }
   };
+
+  //Get damage attainable with the given amount of gold
+  //This just follows the plan until the gold is expended
   this.GetDamage = function(gold) {
     while (this.planPos < this.plan.length && gold >= this.plan[this.planPos].cost) {
       this.curDamage = this.plan[this.planPos].damage;
@@ -405,12 +413,7 @@ function Compute(levels, used, souls, gilded, activity, damageFactor, retAncient
 
   var Simulate = function(noMorgulis) {
     var factors = getFactors(noMorgulis || !used.morgulis);
-    /*
-    TODO investigate curTime value as a function of iris & http://www.reddit.com/r/ClickerHeroes/comments/2o2pbi/rivsoft_calculator_unofficial_017_version/cmja5lr
     var curGold = (levels.khrysos ? Heroes[levels.khrysos - 1].cost : 0) + 10 * MonsterLife(1 + levels.iris) * MonsterGoldFactor(1 + levels.iris);
-    var curTime = 60;
-    //*/
-    var curGold = (levels.khrysos ? Heroes[levels.khrysos - 1].cost : 0);
     var curTime = 0;
     var curSouls = 0;
     var solomon = 1 + 0.01 * SolomonValue(levels.solomon);
@@ -459,7 +462,24 @@ function Compute(levels, used, souls, gilded, activity, damageFactor, retAncient
       var bestInfo = null;
       var bestFactor = 0;
       for (var k in Ancients) {
-        if (Ancients.hasOwnProperty(k) && levels[k] && k != "morgulis" && used[k]) {
+        if (k == "iris" && (levels[k] - 4) % 5 == 0) {
+          //handle iris specially (skipping boss levels, which break the iterative optimizer)
+          var price = 0;
+          price = AncientPrice(k, levels[k] + 2) + AncientPrice(k, levels[k] + 1);
+          if (souls >= price && (!Ancients[k].maxlvl || levels[k] < Ancients[k].maxlvl)) {
+            levels[k] += 2;
+            souls -= price;
+            var info = Simulate();
+            var factor = (info.ratio - current.ratio) / price;
+            if (factor > bestFactor) {
+              bestAncient = k;
+              bestInfo = info;
+              bestFactor = factor;
+            }
+          levels[k] -= 2;
+          souls += price;
+          }
+        } else if (Ancients.hasOwnProperty(k) && levels[k] && k != "morgulis" && used[k]) {
           var price = AncientPrice(k, levels[k] + 1);
           if (souls >= price && (!Ancients[k].maxlvl || levels[k] < Ancients[k].maxlvl)) {
             levels[k] += 1;
@@ -479,9 +499,16 @@ function Compute(levels, used, souls, gilded, activity, damageFactor, retAncient
       if (!bestAncient) {
         break;
       }
-      levels[bestAncient] += 1;
+      if (bestAncient == "iris" && (levels[k] - 4) % 5 == 0) {
+        souls -= AncientPrice(bestAncient, levels[bestAncient] + 1);
+        souls -= AncientPrice(bestAncient, levels[bestAncient] + 2);
+        levels[bestAncient] += 2;
+      } else {
+        souls -= AncientPrice(bestAncient, levels[bestAncient] + 1);
+        levels[bestAncient] += 1;
+      }
+
       current = bestInfo;
-      souls -= AncientPrice(bestAncient, levels[bestAncient]);
 
       count++;
       if (feedback && !(count % 10)) {
