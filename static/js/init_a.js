55b2703c2db5af1ba008ec07452de73915118d13
pcrane <pcrane@goscoutgo.com>
2015-07-07 12:57:07 -0400

    Updated page to load version .19

--- static/js/init_a.js
+++ static/js/init_a.js
@@ -6,7 +6,7 @@ var game_template = _.template(
 var game_template_data = {
     height: 640,
     width: 1136,
-    ch_version: 2492
+    ch_version: 3670
 };
 $('#game_template_anchor').after(game_template(game_template_data));
 
