42cdd4d782b50bb72aed9d6d9ef9cf7cfb58cab6
pcrane <pcrane@goscoutgo.com>
2015-07-20 12:47:48 -0400

    Added angular app. Also added thumb calculator to it.
    Haven't validated all css/source links yet.

--- angular/calcs.js
+++ angular/calcs.js
@@ -6,12 +6,90 @@ var clickerCalcs = angular.module('clickerCalcApp', []);
 //////////////////
 // controllers  //
 //////////////////
+function calc_base(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    return Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level);
+};
+function calc_morgulis(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then square it
+    return Math.pow(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level),  2);
+};
+function calc_gold(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then .93 & round up
+    return Math.ceil(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level) * .93);
+};
+function calc_clicks(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then .5 & round up
+    return Math.ceil(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level) * .5);
+};
+function calc_jugg(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then .5 & round up
+    return Math.ceil(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level) * .2);
+};
+function calc_solomon(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then .5 & round up
+    return Math.ceil(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level) * .9);
+};
+function calc_iris(ancients, player_ancients) {
+    var siya_id = ancients.siyalatas.id;
+    var argaiv_id = ancients.argaiv.id;
+    var player_argaiv_lvl = _.get(player_ancients, argaiv_id+'.level');
+    var player_siya_lvl = _.get(player_ancients, siya_id+'.level');
+    // get max of argaiv/siya, then -300 & set min at 99
+    return Math.max(Math.max(player_ancients[siya_id].level, player_ancients[argaiv_id].level) - 300, 99);
+};
+
 clickerCalcs.controller('ClickerCalcCtrl', function($scope) {
     $scope.known_ancients = Ancients;
+    $scope.known_ancients_names = _.keys(Ancients);
+
+    // model
+    $scope.ancient_data_by_id = _.reduce(_.values(Ancients), function(ancients_by_id, ancient) {
+        ancients_by_id[ancient.id] = ancient;
+        ancients_by_id[ancient.id].current = 0;
+        ancients_by_id[ancient.id].target = 0;
+        return ancients_by_id;
+    }, {});
+
     $scope.game_data = {
         height: 640,
         width: 1136,
         ch_version: 3670
+    };
+
+    $scope.thumb_funcs = {
+        5: calc_base,
+        28: calc_base,
+        16: calc_morgulis,
+        4: calc_gold,
+        9: calc_gold,
+        10: calc_gold,
+        15: calc_clicks,
+        19: calc_clicks,
+        29: calc_jugg,
+        3: calc_solomon,
+        30: calc_iris
     };
 
     $scope.game_visible = true;
@@ -45,12 +123,52 @@ clickerCalcs.controller('ClickerCalcCtrl', function($scope) {
         var data = $.parseJSON(atob(txt));
         console.log(data);
         $scope.data = data;
+        $scope.post_import();
     };
+    $scope.post_import = function() {
+        // get the new data per-ancient
+        var imported_ancient_data = $scope.data.ancients.ancients;
+        _.each(imported_ancient_data, function(ancient, ancient_id) {
+            // update the current 'current' value
+            var cur_ancient = _.get($scope.ancient_data_by_id, ancient.id, false);
+            if(cur_ancient === false) return;
+            cur_ancient.current = ancient.level;
+
+            // if we have a fn for the ancient, calc the new target
+            var calc_function = _.get($scope.thumb_funcs, cur_ancient.id, false);
+            if(calc_function === false) return;
+            cur_ancient.target = calc_function(
+                $scope.known_ancients, imported_ancient_data);
+        });
+    };
+    $scope.multipliers = function(save_data) {
+        var mult = 1;
+        for (var k in data.achievements) {
+           if (data.achievements[k]) {
+                if (Achievements.hasOwnProperty(k)) {
+                    mult *= 1 + 0.01 * Achievements[k];
+                } else {
+                    mult *= 1.05;
+                }
+            }
+        }
+        AchievementMultiplier = mult;
+        for (var k in data.upgrades) {
+            if (data.upgrades[k] && Upgrades.hasOwnProperty(k)) {
+                mult *= 1 + 0.01 * Upgrades[k];
+            }
+        }
+        for (var k in Upgrades) {
+            AchievementMultiplier *= (1 + 0.01 * Upgrades[k]);
+        }
+        AbaddonMultiplier = data.allDpsMultiplier / mult;
+    }
 
     // load chat tab
     var chat_template = _.template($("script#chat_template").html());
     $('#jayeeyeechat').append(chat_template());
   });
+
 ////////////////////
 //    app setup   //
 ////////////////////
